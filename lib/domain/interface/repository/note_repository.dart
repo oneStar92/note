3aea360f398f4031eeb9c172eae3d434b24e04b0
Ayaan <onestar7984@gmail.com>
2024-01-11 21:40:57 +0900

    feat: NoteRepository 구현

---
+++ lib/domain/interface/repository/note_repository.dart
@@ -0,0 +1,9 @@
+import 'package:note/common/result.dart';
+import 'package:note/domain/model/note.dart';
+
+abstract interface class NoteRepository {
+  Future<Result<void>> create({required Note note});
+  Future<Result<List<Note>>> read();
+  Future<Result<void>> update({required Note note});
+  Future<Result<void>> delete({required Note note});
+}
