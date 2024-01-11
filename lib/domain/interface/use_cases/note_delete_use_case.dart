f35563aca87aa7d606b347d1736ad3791905b0f3
Ayaan <onestar7984@gmail.com>
2024-01-11 22:07:34 +0900

    refactor: DeleteUseCase query Type 변경

--- lib/domain/interface/use_cases/note_delete_use_case.dart
+++ lib/domain/interface/use_cases/note_delete_use_case.dart
@@ -1,3 +1,4 @@
 import 'package:note/domain/interface/use_cases/use_case.dart';
+import 'package:note/domain/model/note.dart';
 
-abstract interface class NoteDeleteUseCase extends UseCase<String, void> {}
+abstract interface class NoteDeleteUseCase extends UseCase<Note, void> {}
