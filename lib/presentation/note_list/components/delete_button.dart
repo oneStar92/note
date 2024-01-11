8f18dcee807be9e7dbc4a25a7ac497f712103a19
Ayaan <onestar7984@gmail.com>
2024-01-11 22:44:19 +0900

    feat: DeleteButton 구현

---
+++ lib/presentation/note_list/components/delete_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:note/presentation/interface/base_view.dart';
+
+final class DeleteButton extends BaseView {
+  final Color _color;
+
+  const DeleteButton({
+    super.key,
+    super.onClick,
+    required Color color,
+  }) : _color = color;
+
+  @override
+  Widget createContent(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return SizedBox(
+        height: constraints.maxHeight,
+        width: constraints.maxHeight,
+        child: Icon(
+          Icons.delete,
+          color: _color,
+        ),
+      );
+    });
+  }
+}
