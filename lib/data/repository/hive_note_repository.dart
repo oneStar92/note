d2e79631ab6b984e6243045585e8e75cf8b9b19d
Ayaan <onestar7984@gmail.com>
2024-01-11 21:48:48 +0900

    feat: HiveNoteRepository 구현

---
+++ lib/data/repository/hive_note_repository.dart
@@ -0,0 +1,68 @@
+import 'package:note/common/result.dart';
+import 'package:note/domain/interface/repository/note_repository.dart';
+import 'package:note/domain/model/note.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+final class HiveNoteRepository implements NoteRepository {
+  Box<Note>? _box;
+
+  HiveNoteRepository() {
+    _configure();
+  }
+
+  void _configure() async {
+    _box = await Hive.openBox<Note>('note');
+  }
+
+  @override
+  Future<Result<void>> create({required Note note}) async {
+    final box = _box;
+    if (box != null) {
+      try {
+        return Result.success(await _box?.add(note));
+      } catch (e) {
+        return Result.error(e.toString());
+      }
+    } else {
+      return const Result.error('Box is null');
+    }
+  }
+
+  @override
+  Future<Result<void>> delete({required Note note}) async {
+    final box = _box;
+    if (box != null) {
+      try {
+        return Result.success(await note.delete());
+      } catch (e) {
+        return Result.error(e.toString());
+      }
+    } else {
+      return const Result.error('Box is null');
+    }
+  }
+
+  @override
+  Future<Result<List<Note>>> read() async {
+    final box = _box;
+    if (box != null) {
+      return Result.success(box.values.toList());
+    } else {
+      return const Result.error('Box is null');
+    }
+  }
+
+  @override
+  Future<Result<void>> update({required Note note}) async {
+    final box = _box;
+    if (box != null) {
+      try {
+        return Result.success(await note.save());
+      } catch (e) {
+        return Result.error(e.toString());
+      }
+    } else {
+      return const Result.error('Box is null');
+    }
+  }
+}
