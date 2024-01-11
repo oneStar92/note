ab9d624e3349c1962a92da14e555fe88310d5447
Ayaan <onestar7984@gmail.com>
2024-01-12 01:22:49 +0900

    refactor: NoteScreen 상태관리 구현

--- lib/presentation/note/note_screen.dart
+++ lib/presentation/note/note_screen.dart
@@ -1,88 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:note/presentation/note/components/color_picker.dart';
 import 'package:note/presentation/note/components/save_button.dart';
+import 'package:note/presentation/note/note_view_model.dart';
+import 'package:provider/provider.dart';
 
 final class NoteScreen extends StatelessWidget {
   const NoteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.indigo,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Stack(
-            children: [
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: Column(
-                  children: [
-                    Row(
+    return Consumer<NoteViewModel>(
+      builder: (context, viewModel, child) {
+        return Scaffold(
+          backgroundColor: Color(viewModel.backgroundColor),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: Column(
                       children: [
-                        SizedBox(
-                          height: 40,
-                          width: 100,
-                          child: ColorPickerView(),
+                        Row(
+                          children: [
+                            SizedBox(
+                              height: 40,
+                              width: 100,
+                              child: ColorPickerView(
+                                onPickedColor: (Color color) {
+                                  print(color);
+                                  context.read<NoteViewModel>().backgroundColor = color.value;
+                                },
+                                title: '배경',
+                                color: Color(viewModel.backgroundColor),
+                                fontColor: Color(viewModel.fontColor),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            SizedBox(
+                              height: 40,
+                              width: 100,
+                              child: ColorPickerView(
+                                onPickedColor: (Color color) => context.read<NoteViewModel>().fontColor = color.value,
+                                title: '폰트',
+                                color: Color(viewModel.fontColor),
+                                fontColor: Color(viewModel.fontColor),
+                              ),
+                            ),
+                          ],
                         ),
-                        SizedBox(width: 20,),
-                        SizedBox(
-                          height: 40,
-                          width: 100,
-                          child: ColorPickerView(),
+                        TextField(
+                          maxLines: 1,
+                          controller: TextEditingController(text: viewModel.title),
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(context.read<NoteViewModel>().fontColor),
+                          ),
+                          textAlign: TextAlign.left,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            hintText: '제목을 입력하세요...',
+                            hintStyle: TextStyle(
+                              fontSize: 24,
+                              color: Color(context.read<NoteViewModel>().fontColor).withOpacity(0.5),
+                            ),
+                          ),
+                          onChanged: (value) {
+                            context.read<NoteViewModel>().title = value;
+                          },
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: TextEditingController(text: viewModel.content),
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(context.read<NoteViewModel>().fontColor),
+                            ),
+                            textAlign: TextAlign.left,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              hintText: '내용을 입력하세요...',
+                              hintStyle: TextStyle(
+                                fontSize: 16,
+                                color: Color(context.read<NoteViewModel>().fontColor).withOpacity(0.5),
+                              ),
+                            ),
+                            onChanged: (value) {
+                              context.read<NoteViewModel>().content = value;
+                            },
+                          ),
                         ),
                       ],
                     ),
-                    TextField(
-                      maxLines: 1,
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.amber,
-                      ),
-                      textAlign: TextAlign.left,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        hintText: '제목을 입력하세요...',
-                        hintStyle: TextStyle(
-                          fontSize: 24,
-                          color: Colors.amber.withOpacity(0.5),
-                        ),
-                      ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: SaveButton(
+                      onClick: () {
+                        context.read<NoteViewModel>().save(
+                              onError: () {},
+                              onComplete: () {
+                                Navigator.pop(context);
+                              },
+                            );
+                      },
                     ),
-                    Expanded(
-                      child: TextField(
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.amber,
-                        ),
-                        textAlign: TextAlign.left,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          hintText: '내용을 입력하세요...',
-                          hintStyle: TextStyle(
-                            fontSize: 16,
-                            color: Colors.amber.withOpacity(0.5),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Positioned(
-                bottom: 0,
-                  right: 0,
-                  child: SaveButton(),
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
