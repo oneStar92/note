f4c0f81446a55d31ff841bab7130e71b9abe00bc
Ayaan <onestar7984@gmail.com>
2024-01-11 23:43:40 +0900

    feat: SaveButton 구현

---
+++ lib/presentation/note/components/save_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:note/presentation/interface/base_view.dart';
+
+final class SaveButton extends BaseView {
+  const SaveButton({
+    super.key,
+    super.onClick,
+  });
+
+  @override
+  Widget createContent(BuildContext context) {
+    return Container(
+      height: 70,
+      width: 70,
+      decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+      padding: const EdgeInsets.all(15),
+      child: const FittedBox(
+        child: Icon(Icons.save),
+      ),
+    );
+  }
+}
