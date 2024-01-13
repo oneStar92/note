4a9c2d6ec4631857258bdd4479e62ae50ea2ce62
Ayaan <onestar7984@gmail.com>
2024-01-13 12:21:13 +0900

    refactor: ViewModelProvider 수정

--- lib/di/view_model_provider.dart
+++ lib/di/view_model_provider.dart
@@ -1,9 +1,8 @@
 import 'package:note/data/repository/hive_note_repository.dart';
 import 'package:note/domain/model/note.dart';
-import 'package:note/domain/use_cases/note_create_use_case_impl.dart';
 import 'package:note/domain/use_cases/note_delete_use_case_impl.dart';
 import 'package:note/domain/use_cases/note_read_use_case_impl.dart';
-import 'package:note/domain/use_cases/note_update_use_case_impl.dart';
+import 'package:note/domain/use_cases/note_save_use_case_impl.dart';
 import 'package:note/presentation/note/note_screen.dart';
 import 'package:note/presentation/note/note_view_model.dart';
 import 'package:note/presentation/note/note_view_state.dart';
@@ -21,31 +20,18 @@ final noteListViewModelProvider = ChangeNotifierProvider<NoteListViewModel>(
   child: const NoteListScreen(),
 );
 
-// final noteViewModelProvider = ChangeNotifierProvider<NoteViewModel>(
-//   create: (_) {
-//     final repository = HiveNoteRepository.shared;
-//     final createUseCase = NoteCreateUseCaseImpl(repository: repository);
-//     final updateUseCase = NoteUpdateUseCaseImpl(repository: repository);
-//     return NoteViewModel(createUseCase: createUseCase, updateUseCase: updateUseCase);
-//   },
-//   builder: (context, child) {
-//     return NoteScreen(title: context.read<NoteViewModel>().title, content: context.read<NoteViewModel>().content);
-//   },
-// );
-
 ChangeNotifierProvider<NoteViewModel> getNoteViewModelProvider({Note? note}) {
-  return ChangeNotifierProvider<NoteViewModel>(create: (_) {
-    final repository = HiveNoteRepository.shared;
-    final createUseCase = NoteCreateUseCaseImpl(repository: repository);
-    final updateUseCase = NoteUpdateUseCaseImpl(repository: repository);
-    return NoteViewModel(
-      createUseCase: createUseCase,
-      updateUseCase: updateUseCase,
-      noteViewState: NoteViewState(
-        note: note ?? Note(),
-      ),
-    );
-  }, builder: (context, child) {
-    return NoteScreen();
-  });
+  return ChangeNotifierProvider<NoteViewModel>(
+    create: (_) {
+      final repository = HiveNoteRepository.shared;
+      final noteSaveUseCase = NoteSaveUseCaseImpl(repository: repository);
+      return NoteViewModel(
+        noteSaveUseCase: noteSaveUseCase,
+        noteViewState: NoteViewState(
+          note: note ?? Note(),
+        ),
+      );
+    },
+    child: NoteScreen(),
+  );
 }
