df751b1bf381429eb6f31fca57b89dcf627bfaf4
Ayaan <onestar7984@gmail.com>
2024-01-13 12:21:41 +0900

    refactor: ContentTextFormField 구현

---
+++ lib/presentation/note/components/content_text_form_field.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+final class ContentTextField extends StatelessWidget {
+  final String _hint = '내용을 입력하세요.';
+  final TextEditingController? _textEditingController;
+  final Function(String)? _onChanged;
+  final Color _fontColor;
+
+  const ContentTextField({
+    super.key,
+    TextEditingController? textEditingController,
+    Function(String)? onChanged,
+    required Color fontColor,
+  })  : _textEditingController = textEditingController,
+        _onChanged = onChanged,
+        _fontColor = fontColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      maxLines: 0,
+      maxLength: 1000,
+      controller: _textEditingController,
+      style: TextStyle(
+        fontSize: 16,
+        color: _fontColor,
+      ),
+      textAlign: TextAlign.left,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        hintText: _hint,
+        hintStyle: TextStyle(
+          fontSize: 16,
+          color: _fontColor.withOpacity(0.5),
+        ),
+      ),
+      onChanged: _onChanged,
+    );
+  }
+}
