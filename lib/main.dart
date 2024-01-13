8368ce2ff1698367580302f82498073fd01695e9
Ayaan <onestar7984@gmail.com>
2024-01-13 11:06:12 +0900

    refactor: Main HiveAdapter DAOAdapter로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note/data/dao/note_hive_dao.dart';
 import 'package:note/data/repository/hive_note_repository.dart';
 import 'package:note/di/view_model_provider.dart';
-import 'package:note/domain/model/note.dart';
 
 void main() async {
   await Hive.initFlutter();
-  Hive.registerAdapter(NoteImplAdapter());
+  Hive.registerAdapter(NoteHiveDAOAdapter());
   await HiveNoteRepository.shared.configureBox();
   runApp(const MyApp());
 }
