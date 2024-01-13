d0a6a0a437817a909d94af8b9b421543ee36ad6d
Ayaan <onestar7984@gmail.com>
2024-01-13 11:04:35 +0900

    refactor: NoteRepository update index 제거

--- lib/domain/interface/repository/note_repository.dart
+++ lib/domain/interface/repository/note_repository.dart
@@ -4,6 +4,6 @@ import 'package:note/domain/model/note.dart';
 abstract interface class NoteRepository {
   Future<Result<void>> create({required Note note});
   Future<Result<List<Note>>> read();
-  Future<Result<void>> update({required int index, required Note note});
+  Future<Result<void>> update({required Note note});
   Future<Result<void>> delete({required Note note});
 }
