de1f082953c4ab6efa5abb3bd2c86f32c1872bc3
Ayaan <onestar7984@gmail.com>
2024-01-13 11:05:04 +0900

    refactor: NoteUpdateUseCase index 제거

--- lib/domain/interface/use_cases/note_update_use_case.dart
+++ lib/domain/interface/use_cases/note_update_use_case.dart
@@ -1,5 +1,4 @@
 import 'package:note/domain/interface/use_cases/use_case.dart';
 import 'package:note/domain/model/note.dart';
 
-typedef UpdateQuery = ({int index, Note note});
-abstract interface class NoteUpdateUseCase extends UseCase<UpdateQuery, void> {}
+abstract interface class NoteUpdateUseCase extends UseCase<Note, void> {}
--- lib/domain/use_cases/note_update_use_case_impl.dart
+++ lib/domain/use_cases/note_update_use_case_impl.dart
@@ -1,6 +1,7 @@
 import 'package:note/common/result.dart';
 import 'package:note/domain/interface/repository/note_repository.dart';
 import 'package:note/domain/interface/use_cases/note_update_use_case.dart';
+import 'package:note/domain/model/note.dart';
 
 final class NoteUpdateUseCaseImpl implements NoteUpdateUseCase {
   final NoteRepository _repository;
@@ -10,5 +11,5 @@ final class NoteUpdateUseCaseImpl implements NoteUpdateUseCase {
   }) : _repository = repository;
 
   @override
-  Future<Result<void>> execute({required UpdateQuery query}) => _repository.update(index: query.index, note: query.note);
+  Future<Result<void>> execute({required Note query}) => _repository.update(note: query);
 }
