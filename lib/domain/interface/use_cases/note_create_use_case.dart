425dd06f2971f2e995d0fc24d5ccb9296bec6748
Ayaan <onestar7984@gmail.com>
2024-01-13 12:21:01 +0900

    refactor: 더 이상 쓰이지않는 UseCase 삭제

--- lib/domain/interface/use_cases/note_create_use_case.dart
+++
@@ -1,4 +0,0 @@
-import 'package:note/domain/interface/use_cases/use_case.dart';
-import 'package:note/domain/model/note.dart';
-
-abstract interface class NoteCreateUseCase extends UseCase<Note, void> {}
