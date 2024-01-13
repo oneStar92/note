5c7231d1fd284f580f8cbe2dc63d1fd318be49d4
Ayaan <onestar7984@gmail.com>
2024-01-13 11:55:24 +0900

    refactor: SaveUseCase 구현

---
+++ lib/domain/interface/use_cases/note_save_use_case.dart
@@ -0,0 +1,4 @@
+import 'package:note/domain/interface/use_cases/use_case.dart';
+import 'package:note/domain/model/note.dart';
+
+abstract interface class NoteSaveUseCase extends UseCase<Note, void> {}
---
+++ lib/domain/use_cases/note_save_use_case_impl.dart
@@ -0,0 +1,21 @@
+import 'package:note/common/result.dart';
+import 'package:note/domain/interface/repository/note_repository.dart';
+import 'package:note/domain/interface/use_cases/note_save_use_case.dart';
+import 'package:note/domain/model/note.dart';
+
+final class NoteSaveUseCaseImpl implements NoteSaveUseCase {
+  final NoteRepository _repository;
+
+  const NoteSaveUseCaseImpl({
+    required NoteRepository repository,
+  }) : _repository = repository;
+
+  @override
+  Future<Result<void>> execute({required Note query}) {
+    if (query.primaryKey != null) {
+      return _repository.update(note: query);
+    } else {
+      return _repository.create(note: query);
+    }
+  }
+}
