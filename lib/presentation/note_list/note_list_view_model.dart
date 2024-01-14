cdd817139419bfac235ec3a0659cd8052a3dbcd9
Ayaan <onestar7984@gmail.com>
2024-01-14 16:42:33 +0900

    feat: NoteListViewModel Delete Undo 기능 구현

--- lib/presentation/note_list/note_list_view_model.dart
+++ lib/presentation/note_list/note_list_view_model.dart
@@ -2,18 +2,22 @@ import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:note/domain/interface/use_cases/note_delete_use_case.dart';
 import 'package:note/domain/interface/use_cases/note_read_use_case.dart';
+import 'package:note/domain/interface/use_cases/note_save_use_case.dart';
 import 'package:note/domain/model/note.dart';
 import 'package:note/presentation/note_list/note_list_view_state.dart';
 
 final class NoteListViewModel extends ChangeNotifier {
   final NoteReadUseCase _readUseCase;
+  final NoteSaveUseCase _saveUseCase;
   final NoteDeleteUseCase _deleteUseCase;
   NoteListViewState _state;
 
   NoteListViewModel({
     required NoteReadUseCase readUseCase,
+    required NoteSaveUseCase saveUseCase,
     required NoteDeleteUseCase deleteUseCase,
   })  : _readUseCase = readUseCase,
+        _saveUseCase = saveUseCase,
         _deleteUseCase = deleteUseCase,
         _state = const NoteListViewState() {
     readAll();
@@ -32,16 +36,26 @@ final class NoteListViewModel extends ChangeNotifier {
     );
   }
 
-  void deleteAt(int index) async {
+  Future<void> deleteAt(int index, {required Function() onSuccess}) async {
     final targetNote = _state.notes.elementAtOrNull(index);
     if (targetNote != null) {
       final result = await _deleteUseCase.execute(query: targetNote);
       result.when(
-        success: (_) {
+        success: (note) {
+          _state = _state.copyWith(lastDeletedNote: note);
           readAll();
+          onSuccess();
         },
         error: (_) {},
       );
+    }
+  }
+
+  void undo() async {
+    final lastDeletedNote = _state.lastDeletedNote;
+    if (lastDeletedNote != null) {
+      final result = await _saveUseCase.execute(query: lastDeletedNote);
+      result.when(success: (_) => readAll(), error: (_) {});
     }
   }
 
