d062ede4f2436cbd6766e88f6a799cefc6becd1d
Ayaan <onestar7984@gmail.com>
2024-01-14 12:45:21 +0900

    feat: 색상 선택을 앱바에서 하도록 구현

--- lib/presentation/note/note_screen.dart
+++ lib/presentation/note/note_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:note/presentation/note/components/color_picker.dart';
+import 'package:note/presentation/note/components/color_picker_dialog.dart';
 import 'package:note/presentation/note/components/content_text_form_field.dart';
+import 'package:note/presentation/note/components/popup_setting_button.dart';
 import 'package:note/presentation/note/components/save_button.dart';
 import 'package:note/presentation/note/components/title_text_form_field.dart';
 import 'package:note/presentation/note/note_view_model.dart';
@@ -26,101 +27,106 @@ class _NoteScreenState extends State<NoteScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<NoteViewModel>(builder: (context, viewModel, child) {
-      return Scaffold(
-        backgroundColor: Color(viewModel.backgroundColor),
-        appBar: AppBar(
-          iconTheme: IconThemeData(
-            color: Color(viewModel.borderColor),
+    return Consumer<NoteViewModel>(
+      builder: (context, viewModel, child) {
+        return Scaffold(
+          backgroundColor: Color(viewModel.backgroundColor),
+          appBar: AppBar(
+            iconTheme: IconThemeData(
+              color: Color(viewModel.borderColor),
+            ),
+            backgroundColor: Colors.transparent,
+            scrolledUnderElevation: 0,
+            actions: [
+              PopupSettingButton(
+                onSelected: (menu) {
+                  _showColorPickerDialog(
+                    context: context,
+                    onPickedColor: (color) => menu == SettingMenu.backgroundColor
+                        ? viewModel.backgroundColor = color.value
+                        : viewModel.fontColor = color.value,
+                    color: menu == SettingMenu.backgroundColor
+                        ? Color(viewModel.backgroundColor)
+                        : Color(viewModel.fontColor),
+                  );
+                },
+                backgroundColor: Color(viewModel.backgroundColor),
+                fontColor: Color(viewModel.fontColor),
+              )
+            ],
           ),
-          backgroundColor: Colors.transparent,
-          scrolledUnderElevation: 0,
-        ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          SizedBox(
-                            height: 40,
-                            width: 100,
-                            child: ColorPickerView(
-                              onPickedColor: (Color color) => viewModel.backgroundColor = color.value,
-                              title: '배경',
-                              color: Color(viewModel.backgroundColor),
-                              fontColor: Color(viewModel.fontColor),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                          SizedBox(
-                            height: 40,
-                            width: 100,
-                            child: ColorPickerView(
-                              onPickedColor: (Color color) => viewModel.fontColor = color.value,
-                              title: '폰트',
-                              color: Color(viewModel.fontColor),
-                              fontColor: Color(viewModel.fontColor),
-                            ),
-                          ),
-                        ],
-                      ),
-                      TitleTextFormField(
-                        textEditingController: _titleController,
-                        onChanged: (value) => viewModel.title = value,
-                        fontColor: Color(viewModel.fontColor),
-                      ),
-                      Expanded(
-                        child: ContentTextFormField(
-                          textEditingController: _contentController,
-                          onChanged: (value) => viewModel.content = value,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: Column(
+                      children: [
+                        TitleTextFormField(
+                          textEditingController: _titleController,
+                          onChanged: (value) => viewModel.title = value,
                           fontColor: Color(viewModel.fontColor),
                         ),
-                      ),
-                    ],
+                        Expanded(
+                          child: ContentTextFormField(
+                            textEditingController: _contentController,
+                            onChanged: (value) => viewModel.content = value,
+                            fontColor: Color(viewModel.fontColor),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: SaveButton(
-                    onClick: () async {
-                      final result = await context.read<NoteViewModel>().save();
-                      result.when(
-                        success: (_) {
-                          Navigator.pop(context);
-                        },
-                        error: (e) {
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text(
-                              e,
-                              style: const TextStyle(
-                                fontSize: 24,
-                                color: Colors.white,
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: SaveButton(
+                      onClick: () async {
+                        final result = await context.read<NoteViewModel>().save();
+                        result.when(
+                          success: (_) {
+                            Navigator.pop(context);
+                          },
+                          error: (e) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(
+                                e,
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                            duration: const Duration(seconds: 1),
-                          ));
-                        },
-                      );
-                    },
+                              duration: const Duration(seconds: 1),
+                            ));
+                          },
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      );
-    });
+        );
+      },
+    );
+  }
+
+  void _showColorPickerDialog(
+      {required BuildContext context, required Function(Color) onPickedColor, required Color color}) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return ColorPickerDialog(
+          onPickedColor: onPickedColor,
+          color: color,
+        );
+      },
+    );
   }
 }
