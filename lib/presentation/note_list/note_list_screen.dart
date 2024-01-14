e04f3436def953cf144b36d35dd9057a054290cf
Ayaan <onestar7984@gmail.com>
2024-01-14 13:29:15 +0900

    refactor: ListView Widget 분리하지 않고 Consumer 이용하도록 구현

--- lib/presentation/note_list/note_list_screen.dart
+++ lib/presentation/note_list/note_list_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:note/di/view_model_provider.dart';
+import 'package:note/domain/model/note.dart';
 import 'package:note/presentation/note_list/components/add_button.dart';
-import 'package:note/presentation/note_list/components/note_list_view.dart';
+import 'package:note/presentation/note_list/components/note_list_item.dart';
 import 'package:note/presentation/note_list/note_list_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -26,12 +27,34 @@ final class NoteListScreen extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Stack(
               children: [
-                const Positioned(
+                Positioned(
                   top: 0,
                   left: 0,
                   right: 0,
                   bottom: 0,
-                  child: NoteListView(),
+                  child: ListView.separated(
+                      itemBuilder: (context, index) {
+                        return SizedBox(
+                          height: 80,
+                          width: double.infinity,
+                          child: NoteListItem(
+                            onClick: () => _pushNoteScreen(context: context, note: viewModel.notes[index])
+                                .then((value) => viewModel.readAll()),
+                            onDelete: () => viewModel.deleteAt(index),
+                            title: viewModel.notes[index].title,
+                            preview: viewModel.previewAt(index),
+                            backgroundColor: Color(viewModel.notes[index].backgroundColor),
+                            fontColor: Color(viewModel.notes[index].fontColor),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return const Divider(
+                          color: Colors.transparent,
+                          height: 8.0,
+                        );
+                      },
+                      itemCount: viewModel.notes.length),
                 ),
                 Positioned(
                   bottom: 0,
@@ -50,11 +73,11 @@ final class NoteListScreen extends StatelessWidget {
     });
   }
 
-  Future<void> _pushNoteScreen({required BuildContext context}) {
+  Future<void> _pushNoteScreen({required BuildContext context, Note? note}) {
     return Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (_) => getNoteViewModelProvider(),
+        builder: (_) => getNoteViewModelProvider(note: note),
       ),
     );
   }
