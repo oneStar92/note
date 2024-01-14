63a3e1801def9f1142aa4a86fcbdf34fed097040
Ayaan <onestar7984@gmail.com>
2024-01-14 16:40:10 +0900

    refactor: NoteRepository Delete Success Note Type 반환

--- lib/domain/interface/repository/note_repository.dart
+++ lib/domain/interface/repository/note_repository.dart
@@ -5,5 +5,5 @@ abstract interface class NoteRepository {
   Future<Result<void>> create({required Note note});
   Future<Result<List<Note>>> read();
   Future<Result<void>> update({required Note note});
-  Future<Result<void>> delete({required Note note});
+  Future<Result<Note>> delete({required Note note});
 }
